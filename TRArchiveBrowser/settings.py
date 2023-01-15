3be2db1088892de49e81d124856867e7a1f95778
Rob Hailman <robhailman@gmail.com>
2023-01-14 21:26:51 -0500

    Add view pagination

--- TRArchiveBrowser/settings.py
+++ TRArchiveBrowser/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 
 from pathlib import Path
 from .settings_secret import *
@@ -23,8 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -70,17 +69,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'TRArchiveBrowser.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -116,6 +104,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- archive/views.py
+++ archive/views.py
@@ -7,32 +7,37 @@ from .models import Artist, Genre, Label, Record, Style
 
 
 class RecordListView(ListView):
+    paginate_by = 100
     model = Record
 
 
 class ArtistRecordListView(ListView):
+    paginate_by = 100
+
     def get_queryset(self):
         self.artist = get_object_or_404(Artist, slug=self.kwargs["slug"])
         return Record.objects.filter(artist=self.artist)
 
 
 class LabelRecordListView(ListView):
+    paginate_by = 100
+
     def get_queryset(self):
         self.label = get_object_or_404(Label, slug=self.kwargs["slug"])
         return Record.objects.filter(label=self.label)
 
 
 class GenreRecordListView(ListView):
+    paginate_by = 100
+
     def get_queryset(self):
         self.genre = get_object_or_404(Genre, slug=self.kwargs["slug"])
         return Record.objects.filter(genres=self.genre)
 
 
 class StyleRecordListView(ListView):
+    paginate_by = 100
+
     def get_queryset(self):
         self.style = get_object_or_404(Style, slug=self.kwargs["slug"])
         return Record.objects.filter(styles=self.style)
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the archive index.")
