d2ae25b8f24e05e6fb37684bafd0b2e57f1ac38f
Rob Hailman <robhailman@gmail.com>
2023-01-15 14:23:40 -0500

    Improve importer logging

--- archive/management/commands/import.py
+++ archive/management/commands/import.py
@@ -2,12 +2,23 @@ import argparse
 from csv import DictReader
 
 from django.core.management.base import BaseCommand, CommandError
+from django.utils.text import slugify
 
 from archive import models
 
 
 class Command(BaseCommand):
     help = "Import a csv file of records"
+
+    def check_slug(self, object):
+        slug = object.slug
+        expected_slug = slugify(object.name, allow_unicode=True)
+        if slug != expected_slug:
+            self.stdout.write(
+                self.style.WARNING(
+                    f"{object.__class__.__name__} {object} did not get expected slug. Expected {expected_slug}, got {slug}"
+                )
+            )
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -30,7 +41,10 @@ class Command(BaseCommand):
                 name=row["Artist"].strip()
             )
             if artist_created:
-                self.stdout.write(f"Created artist {artist}")
+                if options["verbosity"] >= 2:
+                    self.stdout.write(f"Created artist {artist}")
+                self.check_slug(artist)
+
             record, record_created = models.Record.objects.get_or_create(
                 catalog_number=row["Cat #"],
                 defaults={"artist": artist, "type_id": options["recordtype"][0]},
@@ -45,7 +59,9 @@ class Command(BaseCommand):
                     )
                     record.genres.add(genre)
                     if genre_created:
-                        self.stdout.write(f"Created genre {genre}")
+                        if options["verbosity"] >= 2:
+                            self.stdout.write(f"Created genre {genre}")
+                        self.check_slug(genre)
             if row["Style(s)"]:
                 for style_name in [x.strip() for x in row["Style(s)"].split(",")]:
                     style, style_created = models.Style.objects.get_or_create(
@@ -53,14 +69,18 @@ class Command(BaseCommand):
                     )
                     record.styles.add(style)
                     if style_created:
-                        self.stdout.write(f"Created style {style}")
+                        if options["verbosity"] >= 2:
+                            self.stdout.write(f"Created style {style}")
+                        self.check_slug(style)
             if row["Label"]:
                 label, label_created = models.Label.objects.get_or_create(
                     name=row["Label"].strip()
                 )
                 record.label = label
                 if label_created:
-                    self.stdout.write(f"Created label {label}")
+                    if options["verbosity"] >= 2:
+                        self.stdout.write(f"Created label {label}")
+                    self.check_slug(label)
             if (
                 row["Original Year of Release"]
                 and row["Original Year of Release"] != "Unknown"
@@ -68,14 +88,16 @@ class Command(BaseCommand):
                 record.release_year = int(row["Original Year of Release"])
             record.save()
             if record_created:
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        f"Created record {record.catalog_number}: {record.artist} - {record.title}"
+                if options["verbosity"] >= 1:
+                    self.stdout.write(
+                        self.style.SUCCESS(
+                            f"Created record {record.catalog_number}: {record.artist} - {record.title}"
+                        )
                     )
-                )
             else:
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        f"Updated record {record.catalog_number}: {record.artist} - {record.title}"
+                if options["verbosity"] >= 2:
+                    self.stdout.write(
+                        self.style.SUCCESS(
+                            f"Updated record {record.catalog_number}: {record.artist} - {record.title}"
+                        )
                     )
-                )
