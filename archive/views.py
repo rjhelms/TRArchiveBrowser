5e729617a67d87869bddb7252f304095a8387813
Rob Hailman <robhailman@gmail.com>
2023-01-15 13:41:19 -0500

    Refactor: filtered views inherit RecordListView

--- archive/views.py
+++ archive/views.py
@@ -1,45 +1,39 @@
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
 from django.views.generic import ListView
-from .models import Artist, Genre, Label, Record, Style
 
-# Create your views here.
+from .models import Artist, Genre, Label, Record, Style
 
 
 class RecordListView(ListView):
     paginate_by = 100
     model = Record
 
+    def prefetch_view(self, queryset):
+        return queryset.prefetch_related("artist", "genres", "styles", "label", "type")
+
     def get_queryset(self):
-        return Record.objects.prefetch_related('artist', 'genres', 'styles', 'label', 'type')
+        return self.prefetch_view(Record.objects)
 
-class ArtistRecordListView(ListView):
-    paginate_by = 100
 
+class ArtistRecordListView(RecordListView):
     def get_queryset(self):
         self.artist = get_object_or_404(Artist, slug=self.kwargs["slug"])
-        return Record.objects.filter(artist=self.artist).prefetch_related('artist', 'genres', 'styles', 'label', 'type')
+        return self.prefetch_view(Record.objects.filter(artist=self.artist))
 
 
-class LabelRecordListView(ListView):
-    paginate_by = 100
-
+class LabelRecordListView(RecordListView):
     def get_queryset(self):
         self.label = get_object_or_404(Label, slug=self.kwargs["slug"])
-        return Record.objects.filter(label=self.label).prefetch_related('artist', 'genres', 'styles', 'label', 'type')
+        return self.prefetch_view(Record.objects.filter(label=self.label))
 
 
-class GenreRecordListView(ListView):
-    paginate_by = 100
-
+class GenreRecordListView(RecordListView):
     def get_queryset(self):
         self.genre = get_object_or_404(Genre, slug=self.kwargs["slug"])
-        return Record.objects.filter(genres=self.genre).prefetch_related('artist', 'genres', 'styles', 'label', 'type')
+        return self.prefetch_view(Record.objects.filter(genres=self.genre))
 
 
-class StyleRecordListView(ListView):
-    paginate_by = 100
-
+class StyleRecordListView(RecordListView):
     def get_queryset(self):
         self.style = get_object_or_404(Style, slug=self.kwargs["slug"])
-        return Record.objects.filter(styles=self.style).prefetch_related('artist', 'genres', 'styles', 'label', 'type')
+        return self.prefetch_view(Record.objects.filter(styles=self.style))
