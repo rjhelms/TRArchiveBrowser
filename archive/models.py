354c1e70c845d15bbec2f6bed20efd68b1e85e7d
Rob Hailman <robhailman@gmail.com>
2023-01-15 14:23:55 -0500

    Fix: unique_slugify handles blank slugs

--- archive/models.py
+++ archive/models.py
@@ -10,25 +10,27 @@ from django.utils.text import slugify
 def unique_slugify(instance, slug):
     model = instance.__class__
     unique_slug = slug
-    while model.objects.filter(slug=unique_slug).exists():
+    while model.objects.filter(slug=unique_slug).exists() or unique_slug == "":
         unique_slug = slug + get_random_string(length=4)
     return unique_slug
+
 
 class NameModel(models.Model):
     name = models.CharField(null=False, blank=False, unique=True, max_length=255)
     slug = models.SlugField(blank=True, unique=True, max_length=255)
 
     def save(self, *args, **kwargs):
-        if self.slug == '':
+        if self.slug == "":
             self.slug = unique_slugify(self, slugify(self.name, allow_unicode=True))
         super(NameModel, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
-    
-    class Meta: 
+
+    class Meta:
         abstract = True
-        ordering = ['name']
+        ordering = ["name"]
+
 
 class Record(models.Model):
     catalog_number = models.PositiveIntegerField(primary_key=True)
@@ -40,15 +42,17 @@ class Record(models.Model):
     release_year = models.PositiveIntegerField(blank=True, null=True)
     label = models.ForeignKey("Label", blank=True, null=True, on_delete=models.PROTECT)
     type = models.ForeignKey("RecordType", on_delete=models.PROTECT)
-    
+
     class Meta:
-        ordering = ['catalog_number']
+        ordering = ["catalog_number"]
 
     def __str__(self):
         return f"{self.artist} - {self.title}"
 
+
 class Artist(NameModel):
     pass
+
 
 class Genre(NameModel):
     pass
